52a6fed96b14ae43adca76cbe0c9254195fe8185
Nenetti <akinori1090@gmail.com>
2020-09-22 11:07:24 +0900

    WIP

--- nenepy/torch/transforms/compose.py
+++ nenepy/torch/transforms/compose.py
@@ -9,23 +9,26 @@ class Compose(transforms.Compose):
 
     def __call__(self, *images):
 
-        if len(images) > 1:
+        if len(images) == 1:
+            for t in self.transforms:
+                images = t(*images)
 
+            return images
+
+        else:
             indexes, tensors = self.is_tensors(images)
+
             for t in self.transforms:
                 tensors = t(*tensors)
+
+            if not isinstance(tensors, (tuple, list)):
+                tensors = (tensors,)
 
             output = list(images)
             for index, tensor in zip(indexes, tensors):
                 output[index] = tensor
 
-            return tensors
-
-        else:
-            for t in self.transforms:
-                images = t(*images)
-
-            return images
+            return output
 
     @staticmethod
     def is_tensors(images):
